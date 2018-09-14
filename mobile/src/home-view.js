cab4ec519248f78ebcce3b28948ee16174b2f25f
Adam Liechty <adam@doubledutch.me>
2018-09-13 22:17:55 -0700

    Update to async binding functions

--- mobile/src/home-view.js
+++ mobile/src/home-view.js
@@ -5,22 +5,31 @@ import client, { Avatar, TitleBar } from '@doubledutch/rn-client'
 
 console.disableYellowBox = true
 export default class HomeView extends PureComponent {
+  state = {}
+  componentDidMount() {
+    client.getCurrentUser().then(currentUser => this.setState({currentUser}))
+    client.getCurrentEvent().then(currentEvent => this.setState({currentEvent}))
+  }
+
   render() {
+    const {currentEvent, currentUser} = this.state
+    if (!currentUser || !currentEvent) return null
+
     return (
       <View>
         <TitleBar title="My Badge" client={client} />
         <View style={s.container}>
           <QRCode style={s.qrcode}
-            value={client.currentUser.identifier}
+            value={currentUser.identifier}
             size={256}
             bgColor='black'
             fgColor='white' />
-          <Text style={s.eventName}>{client.currentEvent.name}</Text>
+          <Text style={s.eventName}>{currentEvent.name}</Text>
           <View style={s.nameRow}>
-            <Avatar client={client} user={client.currentUser} size={25} />
-            <Text style={s.name}>{client.currentUser.firstName} {client.currentUser.lastName}</Text>
+            <Avatar client={client} user={currentUser} size={25} />
+            <Text style={s.name}>{currentUser.firstName} {currentUser.lastName}</Text>
           </View>
-          <Text style={s.identifier}>{client.currentUser.identifier}</Text>
+          <Text style={s.identifier}>{currentUser.identifier}</Text>
         </View>
       </View>
     )
