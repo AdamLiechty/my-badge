8f1da1444c06f5b93f95b656328665734006e71b
Adam Liechty <adam@doubledutch.me>
2018-11-15 16:31:51 -0800

    + linter

--- mobile/src/home-view.js
+++ mobile/src/home-view.js
@@ -6,28 +6,33 @@ import client, { Avatar, TitleBar } from '@doubledutch/rn-client'
 console.disableYellowBox = true
 export default class HomeView extends PureComponent {
   state = {}
+
   componentDidMount() {
-    client.getCurrentUser().then(currentUser => this.setState({currentUser}))
-    client.getCurrentEvent().then(currentEvent => this.setState({currentEvent}))
+    client.getCurrentUser().then(currentUser => this.setState({ currentUser }))
+    client.getCurrentEvent().then(currentEvent => this.setState({ currentEvent }))
   }
 
   render() {
-    const {currentEvent, currentUser} = this.state
+    const { currentEvent, currentUser } = this.state
     if (!currentUser || !currentEvent) return null
 
     return (
       <View>
         <TitleBar title="My Badge" client={client} />
         <View style={s.container}>
-          <QRCode style={s.qrcode}
+          <QRCode
+            style={s.qrcode}
             value={currentUser.identifier}
             size={256}
-            bgColor='black'
-            fgColor='white' />
+            bgColor="black"
+            fgColor="white"
+          />
           <Text style={s.eventName}>{currentEvent.name}</Text>
           <View style={s.nameRow}>
             <Avatar client={client} user={currentUser} size={25} />
-            <Text style={s.name}>{currentUser.firstName} {currentUser.lastName}</Text>
+            <Text style={s.name}>
+              {currentUser.firstName} {currentUser.lastName}
+            </Text>
           </View>
           <Text style={s.identifier}>{currentUser.identifier}</Text>
         </View>
@@ -39,21 +44,21 @@ export default class HomeView extends PureComponent {
 const s = StyleSheet.create({
   container: {
     alignItems: 'center',
-    paddingTop: 10
+    paddingTop: 10,
   },
   eventName: {
-    marginTop: 10
+    marginTop: 10,
   },
   nameRow: {
     flexDirection: 'row',
-    marginTop: 5
+    marginTop: 5,
   },
   name: {
     fontSize: 18,
     fontWeight: 'bold',
-    marginLeft: 7
+    marginLeft: 7,
   },
   identifier: {
-    marginTop: 15
-  }
+    marginTop: 15,
+  },
 })
